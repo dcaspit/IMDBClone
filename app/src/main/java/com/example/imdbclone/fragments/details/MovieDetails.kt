4e915af77868e9eaf6a270a55fb3ded318bdd02a
dcaspit <dcaspit@gmail.com>
2023-09-13 19:59:25 +0300

    Starting work on the details page

--- app/src/main/java/com/example/imdbclone/fragments/details/MovieDetails.kt
+++ app/src/main/java/com/example/imdbclone/fragments/details/MovieDetails.kt
@@ -5,13 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.navigation.fragment.navArgs
+import coil.load
 import com.example.imdbclone.databinding.FragmentMovieDetailsBinding
 
 class MovieDetails : Fragment() {
 
     private val args by navArgs<MovieDetailsArgs>()
-
 
     private var _binding: FragmentMovieDetailsBinding? = null
     private val binding get() = _binding!!
@@ -21,10 +22,15 @@ class MovieDetails : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         try {
-
             _binding = FragmentMovieDetailsBinding.inflate(layoutInflater, container, false)
-
             binding.args = args
+            val coverImgUri = ("https://image.tmdb.org/t/p/w1000_and_h450_multi_faces" + args.movie.poster_path).toUri().buildUpon().scheme("https").build()
+            binding.ivCover.load(coverImgUri)
+
+            val imgUri = ("https://www.themoviedb.org/t/p/w220_and_h330_face" + args.movie.poster_path).toUri().buildUpon().scheme("https").build()
+            binding.ivPoster.load(imgUri)
+
+            binding.executePendingBindings()
             return binding.root
         } catch (e: Exception) {
             e.printStackTrace()
